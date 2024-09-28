37f85cb228bfa56e233a53da075616ab9d93fdd4
lucifer2k3 <113585945+lucifer2k3@users.noreply.github.com>
2024-09-28 17:38:19 +0700

    1st

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,17 +1,27 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     private float speed=8f;
     private float jump=500f;
     private float move ;
     private bool isJumping;
-    private bool isFacingRight=true;
+    private bool isFacingRight = true;
     [SerializeField]private Rigidbody2D rb;
     private SpriteRenderer rend;
     public Animator anim;
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+    public float dashingPower = 40f;
+    private float dashingTime = 0.3f;
+
+    public float dashingCooldown = 1f;
+    private bool isDashing=false;
+    private bool canDash = true;
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
@@ -20,13 +30,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDashing){
+            return;
+        }
         Move();
         Flip();
         Jump();
+        if (Time.time >= nextAttackTime && Input.GetKeyDown(KeyCode.J)){
+            Strike();
+            nextAttackTime = Time.time + 1f/ attackRate;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash && rb.velocity.y==0){
+            StartCoroutine(Dash());
+        }
+        anim.SetFloat("Y_velocity",rb.velocity.y);
     }
-
-
-
 
 
     private void OnCollisionExit2D(Collision2D col){
@@ -53,11 +71,30 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isJumping ==false)
         {
             rb.AddForce(new Vector2(rb.velocity.x,jump));
+            
         }
     }
     private void Move(){
         move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(speed * move, rb.velocity.y);
         anim.SetFloat("Speed",Mathf.Abs(rb.velocity.x));
+    }
+    private void Strike(){
+            anim.SetTrigger("Strike");
+    }
+    private IEnumerator Dash(){
+        anim.SetTrigger("Dash");
+        canDash = false;
+        isDashing = true;
+        if (isFacingRight){
+            rb.velocity = new Vector2(15f*dashingPower, 0f);
+        }
+        else{
+            rb.velocity = new Vector2(-15f*dashingPower, 0f);
+        }
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
